76d9aea0ea6094c7fede12467975e315a2d3ff14
LXMchase <257088928@qq.com>
2019-01-09 15:14:22 +0800

    求10个数中的最大值
    Signed-off-by: LXMchase <257088928@qq.com>

---
+++ 求最大值2019.1.5/求最大值2019.1.5/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,max,arr[10];
+	printf("ÊäÈë10¸öÊý£º\n");
+	for (i = 0; i<=9; i++)
+		scanf("%d", &arr[i]);
+	max = arr[0];
+	for (i = 0; i<=9; i++)
+	{
+		if (arr[i+1] > max)
+			max = arr[i + 1];
+	}
+	printf("max=%d\n", max);
+	system("pause");
+	return 0;
+}
