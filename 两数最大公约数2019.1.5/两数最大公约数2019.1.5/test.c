449680af24f54eb54487b4aeb47154cfae527138
LXMchase <257088928@qq.com>
2019-02-24 23:35:17 +0800

    提交代码
    Signed-off-by: LXMchase <257088928@qq.com>

--- 两数最大公约数2019.1.5/两数最大公约数2019.1.5/test.c
+++ 两数最大公约数2019.1.5/两数最大公约数2019.1.5/test.c
@@ -2,17 +2,25 @@
 #include<stdlib.h>
 int main()
 {
-	int a = 39;
-	int b = 46;
+	int a = 24;
+	int b = 18;
 	int m = 0;
-	if (a > b)
+	//方法一
+	//使a为大值，b为小值
+	//if (a > b)
+	//{
+	//	m = a;
+	//	a = b;
+	//	b = m;
+	//}
+	//for (m=a; a%m || b%m; m--);//最大公约数一定小于等于b，直到当a%m为0的前提下b%m为0，则可以找到最大公约数
+	//方法二：辗转相除法
+	while (m = a % b)
 	{
-		m = a;
 		a = b;
 		b = m;
 	}
-	for (m=a; a%m || b%m; m--);
-	printf("%d和%d的最大公约数为%d\n", a, b, m);
+	printf("最大公约数为%d\n",b);
 	system("pause");
 	return 0;
 }
